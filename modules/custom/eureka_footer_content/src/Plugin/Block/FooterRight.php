2f4f7c91bc9fdb143f620ecd2fc593048dbe281d
Rick Hastie <rhastie@austin.utexas.edu>
2017-05-12 13:00:10 -0500

    featurize the config settings for adminimal, bootstrap and eureka themes; add featues module, config_update and adminimal_theme

---
+++ modules/custom/eureka_footer_content/src/Plugin/Block/FooterRight.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\eureka_footer_content\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a Footer Block.
+ *
+ * @Block(
+ *   id = "footer_right_block",
+ *   admin_label = @Translation("Footer Right Block"),
+ * )
+ */
+class FooterRight extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+return [
+      '#theme' => 'footer_content_right',
+      '#name' => 'Mark Fullmer',
+      '#year' => date('Y'),
+      // '#attached' => [
+      //   'css' => [
+      //     drupal_get_path('module', 'acme') . '/assets/css/acme.css'
+      //   ]
+      // ]
+    ];
+  }
+
+}
