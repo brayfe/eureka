c0194429e7ef3fbf0b3d26d0cbe2e34588d48c88
Francine Bray <fbray@austin.utexas.edu>
2017-08-18 18:35:26 -0500

    Create bookmark link service.

---
+++ modules/custom/features/eureka_bookmark_dashboard/src/BookmarkLink.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Drupal\eureka_bookmark_dashboard;
+
+/**
+ * Class BookmarkLink.
+ *
+ * @package Drupal\eureka_bookmark_dashboard
+ */
+class BookmarkLink {
+
+  /**
+   * TODO: update variable declarations.
+   */
+  public static function getLink($type, $id) {
+    $path = '/node/' . $id;
+    $entity_type = 'node';
+    if ($type == 'profile_flag') {
+      $path = '/user/' . $id;
+      $entity_type = 'user';
+    }
+
+    $alias = \Drupal::service('path.alias_manager')->getAliasByPath($path);
+
+    // Redirect anonymous users to the login page.
+    if (\Drupal::currentUser()->isAnonymous()) {
+      // @todo: when we switch to SAML, this link will need to be updated.
+      $link['#markup'] = '<a href="/user/login?destination=' . $alias . '" class="btn btn-info btn-primary">
+          <span class="glyphicon glyphicon-star-empty"></span> Bookmark
+        </a>';
+    }
+    else {
+      $entity = \Drupal::entityTypeManager()->getStorage($entity_type)->load($id);
+      $flag = \Drupal::entityTypeManager()->getStorage('flag')->load($type);
+      $link_type_plugin = $flag->getLinkTypePlugin();
+      $link = $link_type_plugin->getAsFlagLink($flag, $entity);
+    }
+
+    $link['#attached']['library'][] = 'eureka_search_views/eureka-search-views';
+
+    return $link;
+  }
+
+}
