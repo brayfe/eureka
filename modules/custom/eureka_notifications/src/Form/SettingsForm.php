60580b27672ff255cb0c352644eb9c9e1efd51ba
markfullmer <mfullmer@gmail.com>
2017-09-06 13:32:33 -0500

    Add fieldsets on eureka notification configuration form

--- modules/custom/eureka_notifications/src/Form/SettingsForm.php
+++ modules/custom/eureka_notifications/src/Form/SettingsForm.php
@@ -38,79 +38,95 @@ class SettingsForm extends ConfigFormBase {
       '#markup' => 'General configuration options for the site.',
     ];
 
-    $form['tos_text'] = array(
+    $form['tos'] = array(
+      '#type' => 'details',
+      '#title' => t('Terms of Service'),
+      '#open' => TRUE,
+    );
+    $form['tos']['tos_text'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Text displayed in the site terms of service'),
       '#default_value' => $config->get('tos_text'),
+      '#description' => $this->t('Allowed HTML tags: @tags', ['@tags' => '<p> <strong> <em> <br> <a>']),
     );
 
-    $form['notifications_on'] = array(
+    $form['faculty'] = array(
+      '#type' => 'details',
+      '#title' => t('Faculty Notifications'),
+      '#open' => TRUE,
+    );
+    $form['faculty']['notifications_on'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Enable sending of faculty reminder notifications'),
       '#default_value' => $config->get('notifications_on'),
       '#description' => $this->t('On/Off for notifications, below, to be sent.'),
     );
 
-    $form['faculty_profile_stale'] = array(
+    $form['faculty']['faculty_profile_stale'] = array(
       '#type' => 'number',
-      '#title' => $this->t('Faculty Profile Update Threshold'),
+      '#title' => $this->t('Faculty profile update threshold'),
       '#default_value' => $config->get('faculty_profile_stale'),
       '#description' => $this->t('How much time (in days) should elapse before a
         faculty profile which has not been updated be considered stale?'),
     );
-    $form['faculty_notification_frequency'] = array(
+    $form['faculty']['faculty_notification_frequency'] = array(
       '#type' => 'number',
       '#title' => $this->t('Faculty notification frequency'),
       '#default_value' => $config->get('faculty_notification_frequency'),
       '#description' => $this->t('How frequently should faculty be notified (in days) if their stagnant profile has not been updated?'),
     );
 
-    $form['project_stale'] = array(
+    $form['faculty']['project_stale'] = array(
       '#type' => 'number',
-      '#title' => $this->t('Project Update Threshold'),
+      '#title' => $this->t('Project update threshold'),
       '#default_value' => $config->get('project_stale'),
       '#description' => $this->t('How much time (in days) should elapse before a
         project which has not been updated be considered stale?'),
     );
-    $form['project_notification_frequency'] = array(
+    $form['faculty']['project_notification_frequency'] = array(
       '#type' => 'number',
-      '#title' => $this->t('Project Lead notification frequency'),
+      '#title' => $this->t('Project lead notification frequency'),
       '#default_value' => $config->get('project_notification_frequency'),
       '#description' => $this->t('How frequently should project leads be
         notified (in days) if a stagnant project has not been updated?'),
     );
-    $form['project_notification_message'] = array(
+    $form['faculty']['project_notification_message'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Email to update a project'),
       '#default_value' => $config->get('project_notification_message'),
     );
-    $form['profile_notification_message'] = array(
+    $form['faculty']['profile_notification_message'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Email to update a faculty profile'),
       '#default_value' => $config->get('profile_notification_message'),
     );
 
+    $form['students'] = array(
+      '#type' => 'details',
+      '#title' => t('Student Notifications'),
+      '#open' => TRUE,
+    );
     // Relates to sending emails to students about updated content.
-    $form['student_notifications_on'] = array(
+    $form['students']['student_notifications_on'] = array(
       '#type' => 'checkbox',
       '#title' => $this->t('Enable sending of student notifications'),
       '#default_value' => $config->get('student_notifications_on'),
       '#description' => $this->t('On/Off for student notifications, below, to be sent.'),
     );
 
-    $form['student_notification_frequency'] = array(
+    $form['students']['student_notification_frequency'] = array(
       '#type' => 'number',
       '#title' => $this->t('Student notification frequency'),
       '#default_value' => $config->get('student_notification_frequency'),
       '#description' => $this->t('Number of days before notifying students if a faculty member they bookmarked has updated their profile or created a new project.'),
     );
-    $form['student_project_notification_message'] = array(
+    $form['students']['student_project_notification_message'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Notification email text when bookmarked faculty member adds a new project.'),
       '#default_value' => $config->get('student_project_notification_message'),
       '#description' => 'Use the shortcode [faculty] to represent a faculty member name. Use the shortcode [project] to represent a project title. Use [project-url] to represent a direct link to the project.',
     );
-    $form['student_profile_notification_message'] = array(
+    $form['students']['student_profile_notification_message'] = array(
       '#type' => 'textarea',
       '#title' => $this->t('Email text when a bookmarked faculty member profile is updated'),
       '#default_value' => $config->get('student_profile_notification_message'),
